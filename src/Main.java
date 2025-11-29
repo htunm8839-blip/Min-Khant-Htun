5c0edddeb34255911a6532222b36e1eb9274e555
htunm8839-blip <htunm8839@gmail.com>
2025-11-29 12:07:23 +0630

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+//    static void main(string) {
+//
+//        System.out.println(Hello World);
+
+    public static void main(String[] args) {
+        System.out.println("Min Khant Htun");
+    }
+
+
+
+}
+
+
