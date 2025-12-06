630a61305ae74937ef4bf589fb72a61fb42ee486
htunm8839-blip <htunm8839@gmail.com>
2025-12-06 08:39:52 +0630

    U Ye Htun Soe

---
+++ src/Human2.java
@@ -0,0 +1,23 @@
+public class Human2 {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public  void showName(){
+        System.out.println("Hello I am" + name);
+    }
+
+
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,14 @@
 
-//public class Main {
-
-//    static void main(string) {
-//
-//        System.out.println(Hello World);
-
-//    public static void main(String[] args) {
-//        Student student =new Student();
-//        student.setName("Min Khant Htun");
-//        student.showName();
-//        System.out.println("Min Khant Htun);
+public class Main {
 
 
- public class Main {
+    public static void main(String[] args) {
+        Human2 Human2 =new Human2();
+        Human2.setName("U Ye Htun Soe");
+        Human2.showName();
+        System.out.println("U Ye Htun Soe");
 
-     public static void main(String[] args) {
-         Human1 human1 = new Human1("Ye Yint Paing");
-         Human1.showName("Ye Yint Paing");
-         System.out.println("Ye Yint Paing");
-     }
- }
+    }
+}
+
+
