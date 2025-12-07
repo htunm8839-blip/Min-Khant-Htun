51964aed13315a9d2bd2cf6326e0f6ffba7aef63
htunm8839-blip <htunm8839@gmail.com>
2025-12-07 14:23:53 +0630

    Literaldemo

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
 
-//public class Main {
-//
-//
-//    public static void main(String[] args) {
-//        Human3 human3 = new Human3();
-//        human3.setName("Daw Thi Thi Aye");
-//        human3.showName();
-//        System.out.println("Daw Thi Thi Aye");
-//
-//        Human1 human1 = new Human1("Min Khant Htun");
-//        human1.showName();
-//
-//
-//    }
-//}
+
 
 
 
---
+++ src/com/jdc/LiteralDemo.java
@@ -0,0 +1,16 @@
+package com.jdc;
+
+public class LiteralDemo {
+
+    public static void main(String[] args) {
+        boolean b = true;
+        char c = 'a';
+        byte d = 10;
+        short e = 100;
+        int f = 1000;
+        long l = 10000;
+        float m = 10.01f;
+        double n = 100.01918;
+    }
+
+}
