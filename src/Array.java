ee5c09bd36267793a96cf4504c81ba2e6d8d4096
htunm8839-blip <htunm8839@gmail.com>
2025-12-14 06:42:57 +0630

    Array

---
+++ src/Array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Array {
+
+        public static void main(String[] args) {
+
+            int [] array = {1,2,3,4,5,6,7,8,9,10};
+
+            for (int i=0;i<array.length;i++);
+            System.out.println(Arrays.toString(array));
+        }
+
+    }
+
