79433bf518181211885d048e5062924d86a514aa
htunm8839-blip <htunm8839@gmail.com>
2025-12-19 13:32:35 +0630

    switches

--- src/ArrayDemo/ArrayDemo.java
+++ src/ArrayDemo/ArrayDemo.java
@@ -1,23 +1,21 @@
 package ArrayDemo;
 
-//public class ArrayDemo {
-//
-//    public static void main(String[] args) {
-//        Student [] students = new Student[3];
-//
-//        students[0] = new Student("Aung Aung", 20);
-//        students[1] = new Student("Maung Maung", 21);
-//        students[2] = new Student("Thidar", 25);
-//    }
-//}
-//class Student {
-//    String name;
-//    int age;
-//
-//    public Student(String name, int age) {
-//        this.name = name;
-//        this.age = age;
-//    }
-//}
+public class ArrayDemo {
 
+    public static void main(String[] args) {
+        Student [] students = new Student[3];
 
+        students[0] = new Student("Aung Aung", 20);
+        students[1] = new Student("Maung Maung", 21);
+        students[2] = new Student("Thidar", 25);
+    }
+}
+    class Student {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/grade.java
@@ -0,0 +1,10 @@
+public class grade {
+
+    public static void main(String[] args) {
+
+        int[]age= {60};
+
+        }
+
+    }
+
