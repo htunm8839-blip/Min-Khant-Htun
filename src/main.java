6dbfb2fec9d73084a43638e1c50450213fd6e828
htunm8839-blip <htunm8839@gmail.com>
2025-12-25 18:45:57 +0630

    variable

--- src/main.java
+++ src/main.java
@@ -1,2 +1,20 @@
 public class main {
-}
+
+        public static void main(String[] args) {
+
+            String studentName = "Min Khant";
+            int studentID = 157;
+            int studentAge = 19;
+            float studentheight = 5;
+            char studentGrade = 'A';
+
+
+            System.out.println("Student name: " + studentName);
+            System.out.println("Student id: " + studentID);
+            System.out.println("Student age: " + studentAge);
+            System.out.println("Student height: " + studentheight);
+            System.out.println("Student grade: " + studentGrade);
+        }
+    }
+
+
